7072268063d89134092d6cffcf229e6f4f79fe1a
dhruv01234 <1235dhruv@gmail.com>
2024-09-06 10:45:13 +0530

    added

--- quasar.config.js
+++ quasar.config.js
@@ -58,6 +58,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
+      distDir: 'dist/spa',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // transpile: false,
